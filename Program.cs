1495c82a373af66bb59879748a418a99cdd19e49
Felipe <felipec.almeida12@gmail.com>
2023-03-08 18:11:18 -0300

    ´Implementação do Código para Inversão da String

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Digite a palavra que deseja inverter: ");
+string palavra = Console.ReadLine();
+char[] caracteres = palavra.ToCharArray();
+int tamanho = palavra.Length;
+
+for(int i = 0; i < tamanho / 2; i++)
+{
+
+    char temporaria = caracteres[i];
+    caracteres[i] = caracteres[tamanho - 1 - i];
+    caracteres[tamanho - 1 - i] = temporaria;
+
+}
+
+string PalavraInvertida = new string(caracteres);
+Console.WriteLine($"A palavra invertida é: {PalavraInvertida}");
